4d880b4c23bce3d0503b69e6fd45c5c31b4a1433
Shreyansh22485 <119041890+Shreyansh22485@users.noreply.github.com>
2024-01-16 18:54:48 +0530

    Homepage

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const HomeSectionCard = () => {
-    return (
-        <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3'>
-            <div className='w-full h-[10rem]'>
-                <img className='w-full h-full object-cover' src='https://images.unsplash.com/photo-1626865792236-1c2d8e7d6f9e?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ4fHl1ZmZlZSUyMGNvZGV8ZW58MHx8fHwxNjM0NjA5NjQ5&ixlib=rb-1.2.1' />
-            </div>
-            <div className='flex flex-col items-center justify-center w-full h-[10rem]'>
-                <div className='text-2xl font-bold'>Title</div>
-                <div className='text-sm'>Description</div>
-            </div>
+const HomeSectionCard = ({ product }) => {
+  return (
+    <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border">
+      <div className="h-[13rem] w-[10rem] ">
+        <img
+          className="w-full h-full object-cover object-top"
+          src={product.imageUrl}
+        />
+      </div>
+      <div className=" p-4">
+        <h3 className="text-lg font-bold">{product.brand}</h3>
+        <p className="text-sm mt-2 text-gray-900">"{product.title}"</p>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default HomeSectionCard
+export default HomeSectionCard;
